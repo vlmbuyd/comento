ebd3a6900001b3612d62a85f0d331f451699b855
김예안 <kjan0929@naver.com>
2026-01-30 20:15:20 +0900

    refactor(util): 불필요 파라미터 제거
    JSDoc parm 주석 수정

--- todo-list/utils/dateUitls.js
+++ todo-list/utils/dateUitls.js
@@ -1,9 +1,10 @@
 /**
- * 날짜를 "Month Day, Year" 형식으로 포맷팅하는 함수
- * @param {*} today - 날짜 객체
+ * 오늘 날짜를 "Month Day, Year" 형식으로 포맷팅하는 함수
  * @returns - 포맷팅된 날짜 문자열
  */
-export const formatDate = (today = new Date()) => {
+export const formatDate = () => {
+  const today = new Date();
+
   const options = {
     year: 'numeric',
     month: 'long',
